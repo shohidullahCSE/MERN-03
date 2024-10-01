2bde8610466068b480a83ac58bcea57b905268ad
shohidullah <recursiveeducation@gmail.com>
2024-09-30 18:58:25 -0700

    hero-section

---
+++ js-practise/simple-quiz-app/script.js
@@ -0,0 +1,7 @@
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainer = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const scoreContainer = document.getElementById('score-container');
+const scoreElement = document.getElementById('score');
